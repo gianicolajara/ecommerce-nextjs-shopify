5b8bc996965461d562858099ebc9d8794e357835
gianicola <gianicolajara@gmail.com>
2022-05-19 18:26:22 -0400

    add limiter count product on cart

--- components/ChangeNumberProduct.js
+++ components/ChangeNumberProduct.js
@@ -7,16 +7,21 @@ const ChangeNumberProduct = ({ item = {} }) => {
   const { handleUpdateCart } = useContext(CartContext);
 
   const handleSum = () => {
+    console.log(item.quantityAvailable);
+
     handleUpdateCart({
       ...item,
-      quantity: item.quantity >= 0 ? item.quantity + 1 : 0,
+      quantity:
+        item.quantity < item.quantityAvailable
+          ? item.quantity + 1
+          : item.quantityAvailable,
     });
   };
 
   const handleRest = () => {
     handleUpdateCart({
       ...item,
-      quantity: item.quantity > 0 ? item.quantity - 1 : 0,
+      quantity: item.quantity > 1 ? item.quantity - 1 : 1,
     });
   };
 
